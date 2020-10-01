bd7ae56061488bbc6ae50114ac7caec9642ebd05
Avantika29 <68745580+Avantika29@users.noreply.github.com>
2020-10-01 20:34:33 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,26 @@
+const Engine = Matter.Engine;
+const World= Matter.World;
+const Bodies = Matter.Bodies;
+
+var engine, world,ground;
+
+function setup() {
+  createCanvas(800,400);
+
+  engine = Engine.create();
+  world = engine.world;
+
+  ground = new Ground(600,height,1200,20);
+
+  createSprite(400, 200, 50, 50);
+}
+
+function draw() {
+  background(255,255,255);  
+
+  Engine.update(engine);
+
+  ground.display();
+
+  drawSprites();
+}
